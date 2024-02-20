b2ebd9362969608a08be082b3aef60dd6d2e347d
antoshadance <gdemoinoso4ki@gmail.com>
2024-02-20 11:22:06 +0700

    ready20/02

--- src/components/Pages/services/Analytics.jsx
+++ src/components/Pages/services/Analytics.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from '../../Navbar';
 import Contact from '../../Contacts';
+import { Link } from 'react-router-dom';
+import "../../styles/PageNotReady.css"
 
 export default function Analytics() {
+
+    useEffect(()=>{
+        window.scrollTo(0,0)
+    },[])
+
     return(
         <div>
             <Navbar/>
+            <div className='notReady-page'>
+                <div className='notReady-page-content'>
+                    <p>Раздел еще не готов. <br/>Он будет заполнен позже</p>
+                    <Link to="/">Вернуться на главную страницу</Link>
+                </div>
+            </div>
             <Contact/>
         </div>
     )
